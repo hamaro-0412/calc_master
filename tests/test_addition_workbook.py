d4099ca68ae5d957be931a89d08b941f50d09a7a
hamaro-0412 <hamaguchi.shingo.kochi@gmail.com>
2024-09-09 21:33:24 +0900

    足し算クラスのテストプログラムの更新

--- tests/test_addition_workbook.py
+++ tests/test_addition_workbook.py
@@ -2,6 +2,24 @@ import pytest
 
 class TestAdditionWorkbook :
 
+    # 足し算の答えが一桁になるかどうかのテスト 0を除く
+    def test__generate_answer__excluding_0(self) :
+        pass
+
+    # 足し算の答えが一桁になるかどうかのテスト 0を含む
+    def test__generate_answer__including_0(self) :
+        pass
+
+    # 一桁の答えから2つの変数を使った式を求める 0を除く
+    # 答え: 4 -> 2 + 2, 3 + 1, etc
+    def test__generate_fomula__single_digit_answer_and_two_value__excluding_0(self) :
+        pass
+
+    # 一桁の答えから2つの変数を求める 0を含む
+    # 答え: 4 -> 4 + 0, 2 + 2, 3 + 1, etc
+    def test__generate_fomula__single_digit_answer_and_two_value__including_0(self) :
+        pass
+
     # 一桁の足し算の式で、答えも一桁になるかのテスト
     def test__1_digit_formula_1_digit_answer(self) :
         pass
