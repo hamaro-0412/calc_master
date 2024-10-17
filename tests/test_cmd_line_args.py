6c024260698e7fe2d99e71b4762726d7faa96ea4
hamaro-0412 <hamaguchi.shingo.kochi@gmail.com>
2024-10-17 22:11:05 +0900

    期待値の追加

--- tests/test_cmd_line_args.py
+++ tests/test_cmd_line_args.py
@@ -11,8 +11,12 @@ class TestCmdLineArgs :
 
     def test__purse__randint(self) :
         arg = 'randint(1,9)+randint(1,9)'
+
+        assert outs = ['randint(1,9)', '+', 'randint(1,9)']
     
     def test__purse__randint_num_option_cond_option(self, monkeypatch) :
         arg = 'randint(1,9)+randint(1,9) --num=10 --cond=1<ans<10'
         
+        assert outs = ['randint(1,9)', '+', 'randint(1,9)', '--num=10', '--cond=1<ans<10']
+
 
